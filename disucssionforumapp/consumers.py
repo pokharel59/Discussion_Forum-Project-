6bc78948d0cb36e4a81f29be1a86169757fc95a2
pokharel59 <pokhrelaayush59@gmail.com>
2025-02-17 16:24:02 +0545

    Pramit error resloved

--- disucssionforumapp/consumers.py
+++ disucssionforumapp/consumers.py
@@ -37,4 +37,4 @@ class YourConsumer(AsyncWebsocketConsumer):
         message = event["message"]
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({"message": message + " client"}))
